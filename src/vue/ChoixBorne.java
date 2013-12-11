39bce24a221bd6e0a07afc2b81815763b04af899
ChristopheBougere <christophe.bougere@gmail.com>
2013-12-11 11:42:26 +0100

    Finition de la fenêtre choix des bornes

--- src/vue/ChoixBorne.java
+++ src/vue/ChoixBorne.java
@@ -10,39 +10,46 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class ChoixBorne extends JFrame implements AdjustmentListener {
+public class ChoixBorne extends JFrame {
 	
 	private JLabel L_choix, L_nbManuelles, L_nbTelepeage, L_nbAutomatiques;
-	private JScrollBar SB_nbManuelles, SB_nbTelepeage, SB_nbAutomatiques;
-	private JTextField TF_nbManuelles, TF_nbTelepeage, TF_nbAutomatiques;
+	private JSpinner S_nbManuelles, S_nbTelepeage, S_nbAutomatiques;
 	private JButton B_suivant;
 	private BoxLayout BL;
 	private JPanel P_nbManuelles, P_nbTelepeage, P_nbAutomatiques;
 	
 	public ChoixBorne() {
 		super("Choix des bornes");
-
+		
 		L_choix = new JLabel("Choix des bornes :");
 		L_nbManuelles = new JLabel("Nombre de bornes manuelles : ");
 		L_nbTelepeage = new JLabel("Nombre de bornes télépéage : ");
 		L_nbAutomatiques = new JLabel("Nombre de bornes automatiques : ");
 		
 		// Utiliser Spinner
-		SB_nbManuelles = new JScrollBar(JScrollBar.VERTICAL, 2, 0, 0, 9);
-		SB_nbManuelles.addAdjustmentListener(this);
-		SB_nbTelepeage = new JScrollBar(JScrollBar.VERTICAL, 2, 0, 0, 9);
-		SB_nbTelepeage.addAdjustmentListener(this);
-		SB_nbAutomatiques = new JScrollBar(JScrollBar.VERTICAL, 2, 0, 0, 9);
-		SB_nbAutomatiques.addAdjustmentListener(this);
-		
-		TF_nbManuelles = new JTextField("2");
-		TF_nbManuelles.setEditable(false);
-		TF_nbTelepeage = new JTextField("2");
-		TF_nbTelepeage.setEditable(false);
-		TF_nbAutomatiques = new JTextField("2");
-		TF_nbAutomatiques.setEditable(false);
+		S_nbManuelles = new JSpinner();
+		JSpinner.NumberEditor NE_nbManuelles = new JSpinner.NumberEditor(S_nbManuelles);
+		S_nbManuelles.setEditor(NE_nbManuelles);
+		NE_nbManuelles.getModel().setMinimum(0);
+		NE_nbManuelles.getModel().setMaximum(9);
+		NE_nbManuelles.getModel().setValue(2);
+		S_nbTelepeage = new JSpinner();
+		JSpinner.NumberEditor NE_nbTelepeage = new JSpinner.NumberEditor(S_nbTelepeage);
+		S_nbTelepeage.setEditor(NE_nbTelepeage);
+		NE_nbTelepeage.getModel().setMinimum(0);
+		NE_nbTelepeage.getModel().setMaximum(9);
+		NE_nbTelepeage.getModel().setValue(2);
+		S_nbAutomatiques = new JSpinner();
+		JSpinner.NumberEditor NE_nbAutomatiques = new JSpinner.NumberEditor(S_nbAutomatiques);
+		S_nbAutomatiques.setEditor(NE_nbAutomatiques);
+		NE_nbAutomatiques.getModel().setMinimum(0);
+		NE_nbAutomatiques.getModel().setMaximum(9);
+		NE_nbAutomatiques.getModel().setValue(2);
 		
 		B_suivant = new JButton("Suivant");
 
@@ -55,20 +62,17 @@ public class ChoixBorne extends JFrame implements AdjustmentListener {
 		P_nbManuelles = new JPanel();
 		P_nbManuelles.setLayout(new FlowLayout());
 		P_nbManuelles.add(L_nbManuelles);
-		P_nbManuelles.add(TF_nbManuelles);
-		P_nbManuelles.add(SB_nbManuelles);
+		P_nbManuelles.add(S_nbManuelles);
 		
 		P_nbTelepeage = new JPanel();
 		P_nbTelepeage.setLayout(new FlowLayout());
 		P_nbTelepeage.add(L_nbTelepeage);
-		P_nbTelepeage.add(TF_nbTelepeage);
-		P_nbTelepeage.add(SB_nbTelepeage);
+		P_nbTelepeage.add(S_nbTelepeage);
 		
 		P_nbAutomatiques = new JPanel();
 		P_nbAutomatiques.setLayout(new FlowLayout());
 		P_nbAutomatiques.add(L_nbAutomatiques);
-		P_nbAutomatiques.add(TF_nbAutomatiques);
-		P_nbAutomatiques.add(SB_nbAutomatiques);
+		P_nbAutomatiques.add(S_nbAutomatiques);
 		
 		getContentPane().add(L_choix);
 		getContentPane().add(P_nbManuelles);
@@ -78,18 +82,6 @@ public class ChoixBorne extends JFrame implements AdjustmentListener {
 		
 		this.pack();
 		this.setVisible(true);
-		
-	}
-
-	@Override
-	public void adjustmentValueChanged(AdjustmentEvent e) {
-		if(e.getSource() == SB_nbManuelles) {
-			TF_nbManuelles.setText(String.valueOf(SB_nbManuelles.getValue()));
-		} else if(e.getSource() == SB_nbTelepeage) {
-			TF_nbTelepeage.setText(String.valueOf(SB_nbTelepeage.getValue()));
-		} else if(e.getSource() == SB_nbAutomatiques) {
-			TF_nbAutomatiques.setText(String.valueOf(SB_nbAutomatiques.getValue()));
-		}
 		
 	}
 }
