1d452c3a5cb801d76b352100faf19818b7cab0d3
WalidIbarbachane <walid.ibarbachane@ecole.ensicaen.fr>
2013-12-19 12:32:01 +0100

    modification de fenetre alarme

--- src/vue/FenetreAlarme.java
+++ src/vue/FenetreAlarme.java
@@ -5,6 +5,19 @@ package vue;
 import java.awt.BorderLayout;
 import javax.swing.*;
 
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
 /**
  * 
  * @author Ghita BAOUZ
@@ -31,17 +44,22 @@ public class FenetreAlarme extends javax.swing.JFrame {
 	
 	private void initGUI() {
 		try{
+			setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			getContentPane().setLayout(null);
 			_panel = new JPanel();
-			_label = new JLabel();
-			_panel.add(_label);
-			_label.setText("Alarme : "+ _message);
-			_label.setBounds(17, 197, 99, 22);
-			_label.setFont(new java.awt.Font("Khmer OS",1,12));
 
-			getContentPane().add(_panel,BorderLayout.WEST);
-			_panel.setPreferredSize(new java.awt.Dimension(310, 270));
+			getContentPane().add(_panel, "West");
+			_panel.setBounds(0, 0, 490, 76);
+			_panel.setLayout(null);
+			{
+				_label = new JLabel();
+				_panel.add(_label);
+				_label.setText("Alarme : "+ _message);
+				_label.setBounds(78, 26, 373, 25);
+				_label.setFont(new java.awt.Font("Khmer OS",1,12));
+			}
 			pack();
-			this.setSize(500,300);
+			this.setSize(492, 117);
 		} catch (Exception e){
 			e.printStackTrace();
 		}	
