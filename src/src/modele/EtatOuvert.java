48897bae94fa8d9db14567d38b5a9ad12adb5b1b
BaraaElyoussefi <baraa.elyoussefi@ecole.ensicaen.fr>
2014-01-14 09:38:45 +0100

    Commentaires EtatOuvert

--- src/src/modele/EtatOuvert.java
+++ src/src/modele/EtatOuvert.java
@@ -2,6 +2,13 @@ package modele;
 
 import javax.swing.ImageIcon;
 
+
+/**
+ * @author elyoussefi Baraa
+ * @author Bougère Christophe
+ * Cette classe implemente l'interface EtatBorne , et représente l'état où la barrière est  levée.
+ * 
+ * */
 public class EtatOuvert implements EtatBorne {
 
 	@Override
